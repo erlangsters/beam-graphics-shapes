b5e3ce8b0d89c9207d727e90471859531107f43a
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2025-07-28 03:37:00 +0800

    wip - 3d ellipse

---
+++ src/2d/shape_ellipse.erl
@@ -0,0 +1,52 @@
+%%
+%% Copyright (c) 2025, Byteplug LLC.
+%%
+%% This source file is part of a project made by the Erlangsters community and
+%% is released under the MIT license. Please refer to the LICENSE.md file that
+%% can be found at the root of the project repository.
+%%
+%% Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>
+%%
+-module(shape_ellipse).
+-moduledoc """
+To be written.
+
+To be written
+
+To be written.
+""".
+
+-export([
+    solid/0,
+    outline/0,
+    wire/0
+]).
+
+-include_lib("beam_graphics/include/graphics.hrl").
+
+-doc """
+To be written.
+
+To be written.
+""".
+-spec solid() -> #shape2{}.
+solid() ->
+    ok.
+
+-doc """
+To be written.
+
+To be written.
+""".
+-spec outline() -> #shape2{}.
+outline() ->
+    ok.
+
+-doc """
+To be written.
+
+To be written.
+""".
+-spec wire() -> #shape2{}.
+wire() ->
+    ok.
